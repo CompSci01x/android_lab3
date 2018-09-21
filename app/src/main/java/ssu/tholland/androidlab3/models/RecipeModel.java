04068989dfca63824f484f04e3c77498dcd21c58
student <student@iMac-12.local>
2018-09-20 18:39:04 -0700

    working code complete

--- app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
+++ app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
@@ -2,6 +2,25 @@ package ssu.tholland.androidlab3.models;
 
 public class RecipeModel {
 
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
     // class member variables
+    private String recipeName;
+
+    public String getRecipeImageUrl() {
+        return recipeImageUrl;
+    }
+
+    public void setRecipeImageUrl(String recipeImageUrl) {
+        this.recipeImageUrl = recipeImageUrl;
+    }
+
+    private String recipeImageUrl;
 
 }
