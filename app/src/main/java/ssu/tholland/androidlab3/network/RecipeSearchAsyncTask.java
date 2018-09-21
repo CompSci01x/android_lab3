52974221f37a2da55da37fe45c2b83f38c7d78d6
student <student@iMac-6.local>
2018-09-20 20:57:56 -0700

    working code complete

--- app/src/main/java/ssu/tholland/androidlab3/network/RecipeSearchAsyncTask.java
+++ app/src/main/java/ssu/tholland/androidlab3/network/RecipeSearchAsyncTask.java
@@ -8,6 +8,7 @@ import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import ssu.tholland.androidlab3.R;
 import ssu.tholland.androidlab3.models.RecipeModel;
 import ssu.tholland.androidlab3.utilities.RecipeParser;
 
@@ -16,16 +17,46 @@ public class RecipeSearchAsyncTask extends AsyncTask<String, Void, RecipeModel> 
     private final String baseApiUrl = "http://api.yummly.com/v1/api/recipes";
     private final String apiKey = "ec3e34e0bb6801670dbd3dbd02ce7608";
     private final String appId = "4911b643";
+    private RecipeCallbackListener listener;
 
 
     @Override
     protected RecipeModel doInBackground(String... params) {
+        String searchParams = params[0];
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(baseApiUrl).newBuilder();
+        urlBuilder.addQueryParameter("_app_key", apiKey);
+        urlBuilder.addQueryParameter("_app_id", appId);
+        urlBuilder.addQueryParameter("q", searchParams);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(url).build();
+        Response response = null;
+
+        try {
+            //response holds server's answer
+            response = client.newCall(request).execute();
+            if (response != null) {
+                return RecipeParser.recipeFromJson(response.body().string());
+            }
+        } catch (IOException e) {
+            //do something with exception
+        }
 
         return null;
     }
 
+    public void setRecipeCallbackListener(RecipeCallbackListener recipeCallbackListener) {
+        this.listener = recipeCallbackListener;
+    }
+
+    @Override
+    protected void onPostExecute(RecipeModel recipeModel) {
+        super.onPostExecute(recipeModel);
+        listener.onRecipeCallback(recipeModel);
+    }
 
     public interface RecipeCallbackListener {
         void onRecipeCallback(RecipeModel model);
     }
+
 }
