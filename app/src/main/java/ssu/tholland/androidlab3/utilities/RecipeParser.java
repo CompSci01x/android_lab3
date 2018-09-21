5fa09ad14704e06d6a58b40ccffeb4652b08f620
analogHyperdrive <caribblacktip@yahoo.com>
2018-09-21 03:00:06 +0100

    working code complete

--- app/src/main/java/ssu/tholland/androidlab3/utilities/RecipeParser.java
+++ app/src/main/java/ssu/tholland/androidlab3/utilities/RecipeParser.java
@@ -10,15 +10,19 @@ public class RecipeParser {
 
     public static RecipeModel recipeFromJson(String jsonString) {
 
-        RecipeModel model = null;
+        RecipeModel model = new RecipeModel();
 
         try {
             JSONObject response = new JSONObject(jsonString);
             JSONArray matches = response.getJSONArray("matches");
             JSONObject recipe = matches.getJSONObject(0);
-
-            // finish deserializing and creating a model
-
+            String recipeName = recipe.getString("recipeName");
+            JSONArray imageList = recipe.getJSONArray("smallImageUrls");
+            String firstImage = imageList.getString(0);
+            model.setRecipeName(recipeName);
+            model.setRecipeImageUrl(firstImage);
+            
+            return model;
         } catch (JSONException ex) {
             // do something useful with exception
         }
