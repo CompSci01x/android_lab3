84808c34c602bbb70849abbf98bf8fe9742aeec7
Johnathan <john@ph0enix.com>
2018-09-22 15:27:49 -0700

    working code complete

--- app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
+++ app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
@@ -1,7 +1,53 @@
 package ssu.tholland.androidlab3.models;
 
+import android.os.Build;
+
 public class RecipeModel {
 
-    // class member variables
+    private RecipeModel(){
 
+    }
+
+
+    // class member variables
+    private String recipeName;
+    private String recipeImageUrl;
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getRecipeImageUrl() {
+        return recipeImageUrl;
+    }
+
+
+    public static class Builder{
+        private String recipeName;
+        private String recipeImageUrl;
+
+        private RecipeModel instance;
+
+        public Builder(){
+            instance = new RecipeModel();
+        }
+
+        public Builder setRecipeName(String recipeName){
+            this.recipeName = recipeName;
+            return this;
+        }
+
+        public Builder setRecipeImage(String recipeImageUrl){
+            this.recipeImageUrl = recipeImageUrl;
+            return this;
+        }
+
+        public RecipeModel build(){
+            RecipeModel model = new RecipeModel();
+            model.recipeName = this.recipeName;
+            model.recipeImageUrl = this.recipeImageUrl;
+
+            return model;
+        }
+    }
 }
