8f6c4cb5ecaecea7ad068c6572407b066dbeb343
Elliot <elliotn46@gmail.com>
2018-09-27 15:27:05 -0700

    lab 3 finished

--- app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
+++ app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
@@ -2,6 +2,48 @@ package ssu.tholland.androidlab3.models;
 
 public class RecipeModel {
 
-    // class member variables
+    private String recipeName;
+    private String recipeImageUrl;
+    public String getRecipeName() {
+        return recipeName;
+    }
 
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getRecipeImageUrl() {
+        return recipeImageUrl;
+    }
+
+    public void setRecipeImageUrl(String recipeImageUrl) {
+        this.recipeImageUrl = recipeImageUrl;
+    }
+
+
+
+    // class member variables
+    private String recipeDescription;
+    private String recipeImage;
+
+    public String getrecipeName() {
+        return recipeName;
+    }
+
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public String getrecipeImage() {
+        return recipeImage;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
+    }
+
+    public void setrecipeImage(String recipeImage) {
+        this.recipeImage = recipeImage;
+    }
 }
