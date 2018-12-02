a6f217c46a571617ebb9bba3d72f0d2b49b0ec2e
Jake <evansja@sonoma.edu>
2018-12-02 15:41:30 -0800

    working code complete

--- app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
+++ app/src/main/java/ssu/tholland/androidlab3/models/RecipeModel.java
@@ -4,4 +4,27 @@ public class RecipeModel {
 
     // class member variables
 
+
+
+
+
+    private String recipeName;
+    private String recipeImageUrl;
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getRecipeImageUrl() {
+        return recipeImageUrl;
+    }
+
+    public void setRecipeImageUrl(String recipeImageUrl) {
+        this.recipeImageUrl = recipeImageUrl;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
 }
